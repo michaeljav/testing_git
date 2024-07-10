6cfd10e9581a66b36e73984b285debd8f6270cc2
Michael Javier <m.mota@concentra.com.do>
2024-07-09 20:49:57 -0400

    MODIFICADO DESDE EL MAIN BRANCH COMENTARIO

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+//MODIFICADO DESDE EL MAIN BRANCH COMENTARIO
 const data = [
   {
-    identificador_unico: 'AGRICULTURA-449291',
+    identificador_unico: "AGRICULTURA-449291",
     puntaje_institucion_en_xroad: 0,
   },
   {
-    identificador_unico: 'AGRICULTURA-449292',
+    identificador_unico: "AGRICULTURA-449292",
     puntaje_institucion_en_xroad: 1,
   },
   {
-    identificador_unico: 'AGRICULTURA-449293',
+    identificador_unico: "AGRICULTURA-449293",
     puntaje_institucion_en_xroad: 2,
   },
 ];
@@ -21,7 +22,7 @@ function getDifferentInstitutionPuntajXroad(data) {
   const instituWithDiff_puntaje_institucion_en_xroad = [];
   try {
     data?.forEach((obj) => {
-      const siglaInst = obj?.identificador_unico?.split('-')[0]?.trim();
+      const siglaInst = obj?.identificador_unico?.split("-")[0]?.trim();
       //if institution does not exist, I insert it to object
       if (!objs.hasOwnProperty(siglaInst)) {
         objs[siglaInst] = obj?.puntaje_institucion_en_xroad;
@@ -31,10 +32,10 @@ function getDifferentInstitutionPuntajXroad(data) {
         if (objs[siglaInst] !== obj?.puntaje_institucion_en_xroad) {
           instituWithDiff_puntaje_institucion_en_xroad.push({
             rowcode: obj?.identificador_unico,
-            columnName: 'puntaje_institucion_en_xroad',
+            columnName: "puntaje_institucion_en_xroad",
             puntaje_institucion_en_xroad: obj?.puntaje_institucion_en_xroad,
             error:
-              'Una misma institucion no puede tener dos valores distintos en la columna puntaje_institucion_en_xroad',
+              "Una misma institucion no puede tener dos valores distintos en la columna puntaje_institucion_en_xroad",
           });
         }
       }
@@ -44,8 +45,8 @@ function getDifferentInstitutionPuntajXroad(data) {
   } catch (error) {
     return [
       {
-        rowcode: '',
-        columnName: 'puntaje_institucion_en_xroad',
+        rowcode: "",
+        columnName: "puntaje_institucion_en_xroad",
         puntaje_institucion_en_xroad: -1,
         error: `Validando que puntaje_institucion_en_xroad no sea repetido por institución y genero este error: ${error.message}`,
       },
